cbee198be2e828acc6f688ff01e76a3606cbb972
overlookmotel <theoverlookmotel@gmail.com>
2019-12-01 12:33:13 +0000

    `isEmptyObject` method [feat]

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@
 
 // Modules
 const itIs = require('../index'),
-	{isEmptyString, isFullString} = itIs;
+	{isEmptyString, isFullString, isEmptyObject} = itIs;
 
 // Tests
 
@@ -60,5 +60,23 @@ describe('isFullString', () => {
 
 	it('returns false for null', () => {
 		expect(isFullString(null)).toBe(false);
+	});
+});
+
+describe('isEmptyObject', () => {
+	it('returns true for {}', () => {
+		expect(isEmptyObject({})).toBe(true);
+	});
+
+	it('returns false for {a: 1}', () => {
+		expect(isEmptyObject({a: 1})).toBe(false);
+	});
+
+	it("returns false for ''", () => {
+		expect(isEmptyObject('')).toBe(false);
+	});
+
+	it('returns false for null', () => {
+		expect(isEmptyObject(null)).toBe(false);
 	});
 });
